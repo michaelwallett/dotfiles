cc5f2949cd082ad1e021e97e04b6e43eb7f29605
Michael Wallett <mwallett@Michaels-MacBook-Pro.local>
2016-01-01 03:48:55 +0000

    ignoring all .git dirs in sync

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 HOMEBREW_INSTALL_PATH="$DIR/homebrew/install.sh"
 INSTALL_FILES=$(find -H "$DIR" -maxdepth 2 -name 'install.sh' -not -path "$HOMEBREW_INSTALL_PATH")
-DOT_FILES=$(find -H "$DIR" -maxdepth 2 -name '.*' -not -path '.')
+DOT_FILES=$(find -H "$DIR" -maxdepth 2 -name '.*' -not -path '.' -not -path '*.git')
 
 . $HOMEBREW_INSTALL_PATH
 . $INSTALL_FILES
