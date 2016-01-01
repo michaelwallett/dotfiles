e93b4c6903e7c565c4ea7c28b53ed90f63a58163
Michael Wallett <mwallett@Michaels-MacBook-Pro.local>
2016-01-01 12:48:24 +0000

    added private install.sh feature, for stuff I don't want to include in the public repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,16 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 HOMEBREW_INSTALL_PATH="$DIR/homebrew/install.sh"
-INSTALL_FILES=$(find -H "$DIR" -maxdepth 2 -name 'install.sh' -not -path "$HOMEBREW_INSTALL_PATH")
+PRIVATE_INSTALL_PATH="$DIR/private/install.sh"
+INSTALL_FILES=$(find -H "$DIR" -maxdepth 2 -name 'install.sh' -not -path "$HOMEBREW_INSTALL_PATH" -not -path "$PRIVATE_INSTALL_PATH")
 DOT_FILES=$(find -H "$DIR" -maxdepth 2 -name '.*' -not -path '.' -not -path '*.git')
 
 . $HOMEBREW_INSTALL_PATH
 . $INSTALL_FILES
+
+if [ -f $PRIVATE_INSTALL_PATH ]; then
+    . $PRIVATE_INSTALL_PATH
+fi
 
 echo 'Syncing dot files...'
 rsync -avh --no-perms $DOT_FILES ~
