a5b7aca5916b8e15cea12b3f5ac6840928e5de20
Michael Wallett <michael.wallett@gmail.com>
2016-01-01 17:46:19 +0000

    fixed bug where root .gitignore file was getting included in dot files

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 HOMEBREW_INSTALL_PATH="$DIR/homebrew/install.sh"
 PRIVATE_INSTALL_PATH="$DIR/private/install.sh"
 INSTALL_FILES=$(find -H "$DIR" -maxdepth 2 -name 'install.sh' -not -path "$HOMEBREW_INSTALL_PATH" -not -path "$PRIVATE_INSTALL_PATH")
-DOT_FILES=$(find -H "$DIR" -maxdepth 2 -name '.*' -not -path '.' -not -path '*.git')
+DOT_FILES=$(find -H "$DIR" -maxdepth 2 -name '.*' -not -path '.' -not -path '*.git' -not -path "$DIR/.gitignore")
 
 . $HOMEBREW_INSTALL_PATH
 
