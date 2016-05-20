3cdd0588bb99b071c1a6944bab4a63be91c439d2
Michael Wallett <michael.wallett@gmail.com>
2016-05-20 14:00:37 +0100

    Added atom sublime-style-column-selection

--- atom/install.sh
+++ atom/install.sh
@@ -3,6 +3,7 @@
 echo "$(tput setaf 2)Installing atom packages...$(tput sgr 0)"
 
 apm install node-debugger
+apm install sublime-style-column-selection
 apm install emmet
 apm install javascript-snippets
 apm install atom-jade
