f31d012f14d7e7cd316613ffe5c3e16b8f806833
Michael Wallett <michael.wallett@gmail.com>
2023-04-24 17:17:15 +0100

    Added vscode extensions

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,1 +1,11 @@
 code --install-extension ms-python.python
+code --install-extension GraphQL.vscode-graphql-syntax
+code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension yoavbls.pretty-ts-errors
+code --install-extension esbenp.prettier-vscode
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension kamikillerto.vscode-colorize
+code --install-extension shd101wyy.markdown-preview-enhanced
+code --install-extension rangav.vscode-thunder-client
